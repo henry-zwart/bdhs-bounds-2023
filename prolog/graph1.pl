77b1198c594c15f289d969c9d15146e693c69b2b
Henry Zwart <henry.zwart@dragonfly.co.nz>
2023-06-08 15:47:45 +1200

    feat(graph1): add lower bounds to graph

--- prolog/graph1.pl
+++ prolog/graph1.pl
@@ -1,8 +1,8 @@
+% Successors
 parent(a,b,forward).
 parent(a,c,forward).
 parent(b,c,forward).
 parent(c,d,forward).
-parent(c,x,forward).
 parent(d,e,forward).
 parent(d,f,forward).
 parent(e,f,forward).
@@ -11,10 +11,30 @@ parent(f,e,backward).
 parent(f,d,backward).
 parent(e,d,backward).
 parent(d,c,backward).
-parent(d,y,backward).
 parent(c,b,backward).
 parent(c,a,backward).
 parent(b,a,backward).
+
+% Lower bound graph
+forward_lb(a,f,1).
+forward_lb(a,e,2).
+forward_lb(a,d,2).
+forward_lb(a,c,3).
+forward_lb(a,b,4).
+forward_lb(b,f,2).
+forward_lb(b,e,5).
+forward_lb(b,d,4).
+forward_lb(b,c,4).
+forward_lb(c,f,2).
+forward_lb(c,e,4).
+forward_lb(c,d,3).
+forward_lb(d,f,3).
+forward_lb(d,e,4).
+forward_lb(e,f,4).
+
+lb(X,Y,Z) :- forward_lb(X,Y,Z).
+lb(X,Y,Z) :- forward_lb(Y,X,Z).
+
 
 descendant(X,Y,D) :- parent(Y,X,D).
 
